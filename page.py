f88e937f46b8127d3f24c998215893b0d9c7e785
Alex Lubrano <lubranoa@oregonstate.edu>
2025-04-01 13:00:57 -0500

    Edit files to make 1st test pass

--- page.py
+++ page.py
@@ -17,15 +17,16 @@ class BasePage(object):
 
 
 class MainPage(BasePage):
-    """Home page action methods come here. I.e. Python.org"""
+    """Home page action methods come here. I.e. frstaid-ngs.net"""
 
     #Declares a variable that will contain the retrieved text
     search_text_element = SearchTextElement()
 
     def is_title_matches(self):
-        """Verifies that the hardcoded text "Python" appears in page title"""
+        """Verifies that the hardcoded text "Powered by Next Gear Solutions"
+        appears in page title"""
 
-        return "Python" in self.driver.title
+        return "Powered by Next Gear Solutions" in self.driver.title
 
     def click_go_button(self):
         """Triggers the search"""
--- tests.py
+++ tests.py
@@ -16,24 +16,33 @@ class PythonOrgSearch(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-        self.driver.get("http://www.python.org")
+        self.driver.get(BASE_URL)
 
-    def test_search_in_python_org(self):
-        """Tests python.org search feature. Searches for the word "pycon" then
-        verified that some results show up.  Note that it does not look for
-        any particular text in search results page. This test verifies that
-        the results were not empty."""
-
-        #Load the main page. In this case the home page of Python.org.
+    def test_load_login(self):
+        """Tests loading login page for FRSTAid. Verifies the title of the
+        page equals	'Powered by Next Gear Solutions'"""
+        
+        # Load the main page. In this case the home page of frstaid-ngs.net
         main_page = page.MainPage(self.driver)
-        #Checks if the word "Python" is in title
-        self.assertTrue(main_page.is_title_matches(), "python.org title doesn't match.")
-        #Sets the text of search textbox to "pycon"
-        main_page.search_text_element = "pycon"
-        main_page.click_go_button()
-        search_results_page = page.SearchResultsPage(self.driver)
-        #Verifies that the results page is not empty
-        self.assertTrue(search_results_page.is_results_found(), "No results found.")
+        # Checks if the word "Python" is in title
+        self.assertTrue(main_page.is_title_matches(), "website title doesn't match.")
+    
+    # def test_find_company_id_input(self):
+    #     """Tests python.org search feature. Searches for the word "pycon" then
+    #     verified that some results show up.  Note that it does not look for
+    #     any particular text in search results page. This test verifies that
+    #     the results were not empty."""
+
+    #     # Load the main page. In this case the home page of Python.org.
+    #     main_page = page.MainPage(self.driver)
+    #     # Checks if the word "Python" is in title
+    #     self.assertTrue(main_page.is_title_matches(), "website title doesn't match.")
+    #     # Sets the text of search textbox to "pycon"
+    #     # main_page.search_text_element = "pycon"
+    #     # main_page.click_go_button()
+    #     # search_results_page = page.SearchResultsPage(self.driver)
+    #     # Verifies that the results page is not empty
+    #     # self.assertTrue(search_results_page.is_results_found(), "No results found.")
 
     def tearDown(self):
         self.driver.close()
