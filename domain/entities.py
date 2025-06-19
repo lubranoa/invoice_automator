001503afc3f2b9931aa1614f71691793d0ec5425
Alex Lubrano <lubranoa@oregonstate.edu>
2025-06-19 14:55:18 -0500

    Add more to JobInfo dataclass, add new location VO

--- domain/entities.py
+++ domain/entities.py
@@ -1,20 +1,23 @@
 from dataclasses import dataclass
 from value_objects import Department
+from typing import List, Optional
 
 @dataclass
 class JobInfo:
     """"""
     job_number: str
+    location: str
     department: Department
     ins_carrier: str
-    ins_adjuster: str
+    ins_adjuster: str = ""
     client_name: str
     address_line_1: str
-    address_line_2: str
+    address_line_2: str = ""
+    address_line_3: str
     claim_no: str
     
 @dataclass
-class InvoiceInfo:
+class Invoice:
     """"""
     invoice_no: str
     #TODO: add more
--- domain/value_objects.py
+++ domain/value_objects.py
@@ -1,11 +1,20 @@
 from enum import Enum, StrEnum
 
+class Locations(StrEnum):
+    """Locations and codes.
+    """
+    AUS = "144"
+    DFW = "153"
+    HOU = "145"
+    CORP = "20"
+
 class Department(StrEnum):
     """Department codes.
     """
     CON = "CON"
     ELE = "ELE"
     FBR = "FBR"
+    INV = "INV"
     
 class InvoiceType(StrEnum):
     """Types of invoice workflows supported.
